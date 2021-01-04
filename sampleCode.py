b49c67ff60d5b87c2508d6ac483a4f5909abc96f
Yuki Minamoto <minamoto.y.aa@m.titech.ac.jp>
2021-01-04 20:17:24 +0900

    Updated by Kherlen

--- sampleCode.py
+++ sampleCode.py
@@ -11,75 +11,71 @@ K.set_floatx('float64')
 #Change here
 ################################################################################
 #Input file directory
-filenameForWctBar = 'filteredReactionRate.raw'
-filenameForNctBar = 'filteredScalarGradient.raw'
+wct_bar = 'filtered_reaction_rate.raw'
+nct_bar = 'filtered_scalar_gradient.raw'
 
-#Output file directory
-filenameForPhiBar = 'filteredVolumeFractionOfRXN.raw'
+#Filtersize in thermal thickness of a laminar flame (value between 0.50 and 2.00)
+filtersize_dth = 1.0
 
 #Load trained NN model (.h5 file)
-nnModel = load_model('nnModel.h5')
+nn_model = load_model('nn_model.h5')
 
-#Filter size in thermal thickness of a laminar flame (value between 0.50 and 2.00)
-filterSizeInDth = 1.0
+#Output file directory
+phi_bar = 'filtered_volume_fraction_of_local_combustion_mode.raw'
 
 #Laminar flame characteristics
-du      = 0.36408879999999999       #Unburnt mixture density [kg/m^3]
-sl      = 10.400000000000000        #Laminar flame speed [m/s]   
-deltath = 4.90000005811452866E-004  #Thermal thickness of a laminar flame (temperature gradient based) [m]
+du = 0.3641     #Unburnt mixture density [kg/m^3]
+sl = 10.4000    #Laminar flame speed [m/s]   
+dth = 0.00049   #Thermal thickness of a laminar flame (temperature gradient based) [m]
 ################################################################################
 
 #Normalization constants
-normConst_wct = du * sl / deltath  
-normConst_nct = 1.0 / deltath
-normConst_flt = 2.0
+CONSTANT_WCT = du * sl / dth  
+CONSTANT_NCT = 1.0 / dth
+CONSTANT_FILTERSIZE = 2.0
 
 #Check
-print('normConst_wct:',normConst_wct)
-print('normConst_nct:',normConst_nct)
-print('normConst_flt:',normConst_flt)
+print('CONSTANT_WCT:', CONSTANT_WCT)
+print('CONSTANT_NCT:', CONSTANT_NCT)
+print('CONSTANT_FILTERSIZE:', CONSTANT_FILTERSIZE)
 print('')
 
-#Loading wctBar and nctBar
-wctBarPlus     = np.fromfile(filenameForWctBar, count=-1, dtype=np.float64)
-wctBarPlus     = wctBarPlus.reshape(np.shape(wctBarPlus)[0],1)
-nctNormBarPlus = np.fromfile(filenameForNctBar, count=-1, dtype=np.float64)
-nctNormBarPlus = nctNormBarPlus.reshape(np.shape(nctNormBarPlus)[0],1)
+#Loading wct_bar and nct_bar
+wct_bar_plus = np.fromfile(wct_bar, count=-1, dtype=np.float64)
+wct_bar_plus = wct_bar_plus.reshape(np.shape(wct_bar_plus)[0],1)
+nct_bar_plus = np.fromfile(nct_bar, count=-1, dtype=np.float64)
+nct_bar_plus = nct_bar_plus.reshape(np.shape(nct_bar_plus)[0],1)
 
-#Make array for filterSizePlus
-filterSizePlus = np.zeros((np.shape(wctBarPlus)[0],1))
+#Make array for filtersize_dth_plus
+filtersize_dth_plus = np.zeros((np.shape(wct_bar_plus)[0],1))
 
-#Normalizing the data (making wctBarPlus, nctNormBarPlus and filterSizePlus)
-wctBarPlus          = wctBarPlus      / normConst_wct
-nctNormBarPlus      = nctNormBarPlus  / normConst_nct
-filterSizePlus[:,0] = filterSizeInDth / normConst_flt
+#Normalizing the data (making wct_bar_plus, nct_bar_plus and filtersize_dth_plus)
+wct_bar_plus = wct_bar_plus / CONSTANT_WCT
+nct_bar_plus = nct_bar_plus / CONSTANT_NCT
+filtersize_dth_plus[:,0] = filtersize_dth / CONSTANT_FILTERSIZE
 
 #Check 
-print('wctBarPlus_shape:'    ,wctBarPlus.shape)
-print('wctBarPlus_stat:'     ,stats.describe(wctBarPlus))
-print('nctNormBarPlus_shape:',nctNormBarPlus.shape)
-print('nctNormBarPlus_stat:' ,stats.describe(nctNormBarPlus))
-print('filterSizePlus_shape:',filterSizePlus.shape)
-print('filterSizePlus_stat:' ,stats.describe(filterSizePlus))
+print('wct_bar_plus_shape:', wct_bar_plus.shape)
+print('wct_bar_plus_stat:', stats.describe(wct_bar_plus))
+print('nct_bar_plus_shape:', nct_bar_plus.shape)
+print('nct_bar_plus_stat:', stats.describe(nct_bar_plus))
+print('filtersize_dth_plus_shape:', filtersize_dth_plus.shape)
+print('filtersize_dth_plus_stat:', stats.describe(filtersize_dth_plus))
 
-#Concatenating wctBarPlus, nctNormBarPlus and filterSizePlus into X
-X = np.concatenate((wctBarPlus, nctNormBarPlus, filterSizePlus), axis=1)
+#Concatenating wct_bar_plus, nct_bar_plus and filtersize_dth_plus into nn_input
+nn_input = np.concatenate((wct_bar_plus, nct_bar_plus, filtersize_dth_plus), axis=1)
 
 #Prediction 
-y = np.zeros((np.shape(X)[0]))
-y = nnModel.predict(X, batch_size=16, verbose=2)
+nn_output = np.zeros((np.shape(nn_input)[0]))
+nn_output = nn_model.predict(nn_input, batch_size=16, verbose=2)
 
 #Check 
-print('X_shape:',X.shape)
-print('X_stat:' ,stats.describe(X))
-print('y_shape:',y.shape)
-print('y_stat:' ,stats.describe(y))
+print('nn_input_shape:', nn_input.shape)
+print('nn_input_stat:', stats.describe(nn_input))
+print('nn_output_shape:', nn_output.shape)
+print('nn_output_stat:', stats.describe(nn_output))
 print('')
 
 #Prediction result into a file
-fileobj  = open(filenameForPhiBar, mode='wb')
-y.tofile(fileobj)
-fileobj.close
-
-
-
+with open(phi_bar, mode='wb') as f:
+    f.write(nn_output)
